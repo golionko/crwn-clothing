413f2624bfdffb8a115c8b97b63dfc6db8e33201
golionko <agolionko@xtiva.com>
2019-12-05 17:32:57 -0500

    add redux and move user state there

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,8 +3,9 @@ import './header.styles.scss';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
+import { connect } from 'react-redux';
 
-const Header = ({ currentUser }) => (
+const Header = ({currentUser}) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo' />
@@ -23,6 +24,10 @@ const Header = ({ currentUser }) => (
             }
         </div>
     </div>
-)
+);
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+}) 
+
+export default connect(mapStateToProps)(Header);
