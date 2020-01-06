a598ff92c341702ee7d89c333751f6290ae0b0db
golionko <agolionko@xtiva.com>
2020-01-06 15:52:00 -0500

    fix in collections view

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,8 +1,11 @@
 import React from "react";
-import "./collection.styles.scss";
-import CollectionItem from "../../components/collection-item/collection-item.component";
 import { connect } from "react-redux";
+
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
 import { selectCollection } from "../../redux/shop/shop.selectors";
+
+import "./collection.styles.scss";
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
