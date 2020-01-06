6aa1d0626312fef60992513650ae0fa2e40e282f
golionko <agolionko@xtiva.com>
2020-01-06 16:35:21 -0500

    stripe integration

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -7,6 +7,7 @@ import {
 } from "../../redux/cart/cart.selectors";
 import "./checkout.styles.scss";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -30,10 +31,16 @@ const CheckoutPage = ({ cartItems, total }) => (
     {cartItems.map(cartItem => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
-
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp 01/20 - CVV 123
+    </div>
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
